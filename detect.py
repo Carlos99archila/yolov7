764ad290e0117941c77cbd5e3e8fa50e54f4ccb9
Carlos99archila <118935336+Carlos99archila@users.noreply.github.com>
2022-12-29 08:10:19 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -128,9 +128,22 @@ def detect(save_img=False):
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
-                        plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
-                        plot_one_box(xyxy, im0, color=colors2[int(cls)], line_thickness=1000)
+                        #plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
+                        plot_one_box(xyxy, im0, color=colors2[int(cls)], line_thickness=2000)
 
+                ##### Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                        plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
+               
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
