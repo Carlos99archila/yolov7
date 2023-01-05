f2f0703823de112926e028c83f928468d07b6fe5
Carlos99archila <118935336+Carlos99archila@users.noreply.github.com>
2023-01-04 21:01:56 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -130,7 +130,8 @@ def detect(save_img=False):
                         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
                         #plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
                         plot_one_box(xyxy, im0, color=colors2[int(cls)], line_thickness=2500)
-
+                        plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
+                '''
                 ##### Write results
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
@@ -143,7 +144,7 @@ def detect(save_img=False):
                         label = f'{names[int(cls)]} {conf:.2f}'
                         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
                         plot_one_box(xyxy, im0, color=colors[int(cls)], line_thickness=-1)
-               
+                '''
             # Print time (inference + NMS)
             print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
